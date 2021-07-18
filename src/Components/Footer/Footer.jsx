74c88b764fddb401039220364a78efdd66888402
saudhasan762 <azanali152@gmail.com>
2021-07-18 16:48:50 +0530

    Added: added the footer part for the dashboard

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './Footer.scss'
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div className="footer">
+                <p>Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
+            </div>
+        )
+    }
+}
