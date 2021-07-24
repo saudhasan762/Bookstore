a634c56b0b3ab6528edf9c3f0c7dfdb90c7e91ed
saudhasan762 <azanali152@gmail.com>
2021-07-24 09:06:12 +0530

    Updated: updated to wrap the app with the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import store from "./Redux/Store/Store"
 import Routing from "../src/Routes/Routing"
 
 function App() {
   return (
-    <Routing />
+    <>
+    <Provider store={store}>
+      <Routing />
+    </Provider>
+    </>
   );
 }
 
