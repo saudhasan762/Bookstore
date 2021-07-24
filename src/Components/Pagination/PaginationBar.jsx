bfa542162bd95192f8f54150911d06efb40e3db1
saudhasan762 <azanali152@gmail.com>
2021-07-24 09:00:40 +0530

    Added Pagination to the Dashboard to scroll through different Pages

--- src/Components/Pagination/PaginationBar.jsx
+++ src/Components/Pagination/PaginationBar.jsx
@@ -1,1 +1,22 @@
 import Pagination from '@material-ui/lab/Pagination';
+import { Component } from 'react';
+import './PaginationBar.scss'
+
+export default class PaginationBar extends Component{
+
+    changePage = (e, newpage) => {
+        this.props.changepage(e, newpage)
+    };
+
+    render() {
+        return(
+            <>
+                <div className="paginationBlock">
+                    <Pagination count={Math.floor(this.props.books.length / this.props.postsPerPage + 1)}
+                        variant="outlined" shape="rounded" color="primary" onChange={this.changePage}
+                    />
+                </div>
+            </>
+        )
+    }
+}
