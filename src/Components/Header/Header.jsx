87380cf91d7e6c8fcefd527704b237bb44044e55
saudhasan762 <azanali152@gmail.com>
2021-07-26 18:44:18 +0530

    Updated: modified the cart icon to get the count of cart items

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,11 +4,46 @@ import book from "../../Assets/book.svg"
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import StyledBadge from '@material-ui/core/Badge'
+import Profile from "../Profile/Profile"
 import './Header.scss'
+import { searchBook } from "../../Redux/Actions/ActionType";
+import { connect } from "react-redux";
+import BookServices from "../../Services/BookServices";
+const service = new BookServices();
 
-export default class Header extends Component {
+const mapStateToProps = (state) => {
+    console.log(state.cartCount.count);
+    return{
+        count: state.cartCount.count
+    }
+}
+
+class Header extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            search: "",
+            length: ""
+        }
+    }
+
+    handleChange = (e, value) => {
+        console.log(value);
+        this.setState({search: value});
+        this.props.handleSearchBook(value, true);
+        this.props.searchBook(value);
+    }
+
+    componentDidMount(){
+        this.getCartLength();
+    }
+
+    getCartLength = () => {
+        service.getCartItems().then((res) => {
+            this.setState({length: res.data.result.length});
+            console.log(this.state.length);
+        })
     }
 
     render() {
@@ -16,8 +51,8 @@ export default class Header extends Component {
             <>
                 <div className="appbar">
                     <div>
-                        <Link><img src={book} alt=""></img></Link>
-                        <p className="book"><Link style={{ listStyleType: "none", color: 'white', textDecoration: 'none' }}>Bookstore</Link></p>
+                        <Link to="/Dashboard/DisplayBooks"><img src={book} alt=""></img></Link>
+                        <p className="book"><Link to="/Dashboard/DisplayBooks" style={{ listStyleType: "none", color: 'white', textDecoration: 'none' }}>Bookstore</Link></p>
                         <div className="input">
                             <SearchOutlinedIcon className="searchicon" />
                             <input type="text" placeholder="Search" onChange={(e) => this.handleChange(e, e.target.value)} />
@@ -25,10 +60,17 @@ export default class Header extends Component {
                     </div>
                     <div>
                         <div className="pro">
-                            <PermIdentityIcon className="proicon"/>Profile
+                            {/* <PermIdentityIcon className="proicon"/>Profile */}
+                            <Profile />Profile
                         </div>
                         <div className="cart">
-                            <ShoppingCartOutlinedIcon className="carticon" />Cart
+                            <StyledBadge badgeContent={this.props.count} color="white">
+                            <Link to="/Dashboard/Cart">
+                                <ShoppingCartOutlinedIcon className="carticon" />
+                            </Link>
+                            </StyledBadge>
+                            Cart
+                           
                         </div>
                     </div>
                 </div>
@@ -36,3 +78,5 @@ export default class Header extends Component {
         )
     }
 }
+
+export default connect(mapStateToProps, {searchBook})(Header);
